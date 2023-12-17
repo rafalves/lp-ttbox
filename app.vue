a522cab798637423e9becdcd39116c19cd990c5e
Rafael Alves <rafalves.costa@gmail.com>
2023-12-16 23:16:42 -0300

    feat: added seo and lighthouse improvements

--- app.vue
+++ app.vue
@@ -8,3 +8,13 @@
     <SectionCta />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+
+useSeoMeta({
+  title: 'TT.Box | Gerenciamento de Projetos',
+  description: 'O gerenciamento de projetos é o processo de liderar o trabalho de uma equipe para atingir todos os objetivos do projeto dentro das restrições especificadas.',
+  robots: 'noindex, nofollow'
+})
+
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <Html class="antialiased scroll-smooth font-inter transition-all ease-linear duration-300 delay-0">
+  <Html lang="pt-BR" class="antialiased scroll-smooth font-inter transition-all ease-linear duration-300 delay-0">
   <div class="flex flex-col h-full mx-auto">
     <div class="w-full z-50">
       <BaseHeader />
@@ -13,5 +13,4 @@
   </div>
 
   </Html>
-</template>
- 
+</template>  
