977be4f8696ffafb12740af567c56a4c79fb9947
Rafael Alves <rafalves.costa@gmail.com>
2023-12-17 00:40:07 -0300

    feat: added seo and lighthouse improvements

--- app.vue
+++ app.vue
@@ -13,8 +13,7 @@
 
 useSeoMeta({
   title: 'TT.Box | Gerenciamento de Projetos',
-  description: 'O gerenciamento de projetos é o processo de liderar o trabalho de uma equipe para atingir todos os objetivos do projeto dentro das restrições especificadas.',
-  robots: 'index, follow'
+  description: 'O gerenciamento de projetos é o processo de liderar o trabalho de uma equipe para atingir todos os objetivos do projeto dentro das restrições especificadas.'
 })
 
 </script>
